0d5f04940768e1697911b1b74e8422a7158b0cb4
Sonia Heda <_@gmail.com>
2026-09-23 21:53:30 +0300

    Add type annotations

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 import os
+from typing import Any
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(
+        self,
+        exc_type: Any,
+        exc_value: Any,
+        traceback: Any,
+    ) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
